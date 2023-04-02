493fc6e14a7f0eca2cb5b8f84a522d5dea85f014
josearpires <josearpires13@gmail.com>
2023-04-02 16:25:05 -0300

    livro no inicio; cadastro de todos os atributos do livro

--- Biblioteca/src/apresentacao/LivroAp.java
+++ Biblioteca/src/apresentacao/LivroAp.java
@@ -13,7 +13,11 @@ public class LivroAp {
 	
 
 	public void cadastraLivro(Livros elemento) {
-		elemento.setTitulo(JOptionPane.showInputDialog("Digite o tÃ­tulo do livro:"));
+		elemento.setTitulo(JOptionPane.showInputDialog("Digite o título do livro:"));
+		elemento.setIsbn(JOptionPane.showInputDialog("Digite o isbn do livro:"));
+		//elemento.setAutor(JOptionPane.showInputDialog("Digite o autor do livro:"));
+		elemento.setEditora(JOptionPane.showInputDialog("Digite o editora do livro:"));
+		elemento.setAnoPublicacao(Integer.parseInt(JOptionPane.showInputDialog("Digite o ano de publicação do livro:")));
 
 		//teste para github
 			
